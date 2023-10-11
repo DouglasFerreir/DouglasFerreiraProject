35be59a22bee179042e585dee70ffd8b0ad1e5a1
DouglasFerreir <147665862+DouglasFerreir@users.noreply.github.com>
2023-10-11 22:50:59 +0000

    7utfyurdfihobhufyuti

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function cronometro(numeroInicial){
+    console.log(numeroInicial)
+    let proximoNumero = numeroInicial - 1
+    if (proximoNumero > 0){
+        cronometro(proximoNumero)
+    }
+}              
+
+cronometro(100)
