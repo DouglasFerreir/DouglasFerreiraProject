3d931a72b965b01765ec2b1f20475dc160e2e46d
DouglasFerreir <147665862+DouglasFerreir@users.noreply.github.com>
2023-10-19 00:34:50 +0000

    18/10/2023

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
+// mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+// const GatoModelo = mongoose.model('Gato', {
+//     raca: String,
+//     idade: Number,
+//     cor: String
+// })
+
+// const gatoQueAcheiNaRua = new GatoModelo({
+//     raca: "Siames",
+//     idade: 3,
+//     cor: "Branco"
+// })
+
+// gatoQueAcheiNaRua.save().then(() => console.log("Gato salvo com sucesso"))
+
+
+
+
+// exercicio realizado no dia 18 / 10 / 2023
+
+// modelo de produto
+// codigo de barra - string ou Number
+// nome - string
+// preço - number
+
+const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const GatoModelo = mongoose.model('Gato', {
-    raca: String,
-    idade: Number,
-    cor: String
+const Produto = mongoose.model('produto',{
+    codigoDeBarra: Number,
+    nome: String,
+    preço: Number
 })
 
-const gatoQueAcheiNaRua = new GatoModelo({
-    raca: "Siames",
-    idade: 3,
-    cor: "Branco"
+const detergente = new Produto({
+    codigoDeBarra:123456789 ,
+    nome: "detergente",
+    preço: 5
 })
 
-gatoQueAcheiNaRua.save().then(() => console.log("Gato salvo com sucesso"))
+detergente.save()
+
+console.log("Olá")
+
+
+
+Produto.find
+
+
+async function exibeProdutos() {
+  console.log(await Produto.find)) 
+}
+exibeProdutos()
