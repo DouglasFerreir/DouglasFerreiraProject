c683c84a2836892be8690d196b7a47f0da8bc1fb
DouglasFerreir <147665862+DouglasFerreir@users.noreply.github.com>
2023-11-07 00:22:07 +0000

    06.11.23

---
+++ 06.11.2023.ts
@@ -0,0 +1,18 @@
+ export class animal{
+  especie: string
+  coloraçao: string
+  habitat: string
+  age: number
+
+  constructor(especieRecebida: string){
+    this.especie = especieRecebida
+    this.coloracao = string
+    this.habitat = string
+    this.age = number
+  }
+}
+class mamifero extends animal {
+  tempo de gestação = string
+  poe ovo = string
+  subespecie = string
+}
